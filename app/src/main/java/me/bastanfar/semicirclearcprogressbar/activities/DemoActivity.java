5e18f0830733dac213cb97a18fa862dbe1af096e
hadibastanfar <98bastanfar@gmail.com>
2020-05-21 17:39:33 +0430

    Minor bug fix

---
+++ app/src/main/java/me/bastanfar/semicirclearcprogressbar/activities/DemoActivity.java
@@ -0,0 +1,18 @@
+package me.bastanfar.semicirclearcprogressbar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.bastanfar.semicirclearcprogressbar.R;
+
+public class DemoActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
